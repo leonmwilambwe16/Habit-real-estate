61d12ffbaf1cc9030398a646b8b28b27fa8f96a6
leonmwilambwe16 <leonmwilambwe16@gmail.com>
2025-10-17 17:04:13 -0400

    responsive probleme

--- frontend/src/component/Navbar.tsx
+++ frontend/src/component/Navbar.tsx
@@ -3,70 +3,57 @@ import { RiBuilding2Fill } from "react-icons/ri";
 import { FaBell, FaBars } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
 import { NavLink } from 'react-router-dom';
-import {useClerk,UserButton,useUser} from '@clerk/clerk-react';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 import { useUserContext } from '../context/UserContext';
 import '../component.styles/Navbar.scss';
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { loading } = useUserContext();
-  const {openSignIn} = useClerk()
-  const {user} = useUser()
-
-
-  const fallbackImage = "https://via.placeholder.com/40";
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
 
   const toggleMenu = () => setIsOpen(prev => !prev);
+  const closeMenu = () => setIsOpen(false);
 
   return (
     <nav className='navbar-container'>
       <div className="container-box">
 
-        {/* Logo Section */}
-        <div className="logo-box">
-          <span><RiBuilding2Fill /></span>
+        {/* Logo */}
+        <div className="logo-box" onClick={closeMenu}>
+          <RiBuilding2Fill />
           <p>Habit</p>
         </div>
 
         {/* Menu Links */}
         <div className={`menu-bar ${isOpen ? 'open' : ''}`}>
           <ul className="list-menu">
-            <li><NavLink className='list-nav' to="/">Home</NavLink></li>
-            <li><NavLink className='list-nav' to="/list">Map</NavLink></li>
-            <li><NavLink className='list-nav' to="/favorites">Favorites</NavLink></li>
-            <li><NavLink className='list-nav' to="/help">Help</NavLink></li>
-            <li><NavLink className='list-nav' to="/contact">Contact</NavLink></li>
+            <li><NavLink className='list-nav' to="/" onClick={closeMenu}>Home</NavLink></li>
+            <li><NavLink className='list-nav' to="/list" onClick={closeMenu}>Map</NavLink></li>
+            <li><NavLink className='list-nav' to="/favorites" onClick={closeMenu}>Favorites</NavLink></li>
+            <li><NavLink className='list-nav' to="/help" onClick={closeMenu}>Help</NavLink></li>
+            <li><NavLink className='list-nav' to="/contact" onClick={closeMenu}>Contact</NavLink></li>
           </ul>
         </div>
 
-        {/* Right Icons / Auth */}
+        {/* Icons & Auth */}
         <div className="icon-box">
-          <span><FaBell /></span>
-
+          <FaBell />
           {loading ? (
-            <p style={{ color: 'white' }}>Loading...</p>
+            <p>Loading...</p>
+          ) : user ? (
+            <div className='modal'>
+              <p className="greeting">Hi, {user.firstName}</p>
+              <UserButton />
+            </div>
           ) : (
-            <>
-            {user?  <div className='modal'>
-           
-                       <p className="greeting">Hi, {user.firstName} {user.lastName}</p>
-
-                      <UserButton/>
-
-                    </div>
-              :<button onClick={e => openSignIn()} className='auth-btn '>
-                login
-              </button>}
-              
-            </>
+            <button onClick={openSignIn} className='auth-btn'>Login</button>
           )}
-        
 
-          {/* Mobile Menu Toggle */}
-          <div className="btn-open-close">
-            {isOpen
-              ? <IoMdClose onClick={toggleMenu} />
-              : <FaBars onClick={toggleMenu} />}
+          {/* ✅ Hamburger / Close Icon */}
+          <div className="btn-open-close" onClick={toggleMenu}>
+            {isOpen ? <IoMdClose /> : <FaBars />}
           </div>
         </div>
 
